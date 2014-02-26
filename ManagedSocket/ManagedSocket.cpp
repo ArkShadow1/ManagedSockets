fc3fde16097838c5872e165a78cb5477b878d7d2
ArkShadow1 <cody_morrison@hotmail.com>
2014-02-26 18:53:30 -0500

    created the project files

---
+++ ManagedSocket/ManagedSocket.cpp
@@ -0,0 +1,18 @@
+/*
+ * ManagedSocket.cpp
+ *
+ *  Created on: Feb 26, 2014
+ *      Author: cody
+ */
+
+#include "ManagedSocket.h"
+
+ManagedSocket::ManagedSocket() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ManagedSocket::~ManagedSocket() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ ManagedSocket/ManagedSocket.h
@@ -0,0 +1,17 @@
+/*
+ * ManagedSocket.h
+ *
+ *  Created on: Feb 26, 2014
+ *      Author: cody
+ */
+
+#ifndef MANAGEDSOCKET_H_
+#define MANAGEDSOCKET_H_
+
+class ManagedSocket {
+public:
+	ManagedSocket();
+	virtual ~ManagedSocket();
+};
+
+#endif /* MANAGEDSOCKET_H_ */
